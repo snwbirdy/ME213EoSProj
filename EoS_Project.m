e38218549be664bfd4b10ee3b0cd95daef482e24
snwbirdy <58373608+snwbirdy@users.noreply.github.com>
2019-12-03 16:33:39 -0600

    Update EoS_Project.m

--- EoS_Project.m
+++ EoS_Project.m
@@ -14,5 +14,5 @@ S = [11.6, 11.6, 12.1, 13.6, 12.4, 11.4, ...
     9.9, 10.4, 11.7, 12.5, 13.6, 12.1]; % mean 11.9
          % Wind Speed for Height (feet)
 Sh = S.*((H./1000).^(.14));
-         % Distance between turbines (feet)
-D = [100:25:1000];
+         % Temperature avg per Month (MN) (deg F)
+T = [24, 29, 41, 58, 69, 79, 83, 80, 72, 58, 41, 27];
